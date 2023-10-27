ca59121019b7cb6d6b68ae2e79fc95ea38e75aef
mihett05 <mihett05@yandex.ru>
2023-10-27 23:08:15 +0500

    Fixed dataset again again and detection

--- parser/coco.py
+++ parser/coco.py
@@ -11,7 +11,7 @@ def sample_to_coco(sample_name: str, output: io.IOBase):
     result = {}
     sample_path = dataset_path / sample_name
     _, _, files = list(os.walk(sample_path / "frames_ms"))[0]
-    image_w, image_h, _ = cv2.imread(str(sample_path / "frames_rgb" / "0000.png")).shape
+    image_h, image_w, _ = cv2.imread(str(sample_path / "frames_rgb" / "0000.png")).shape
 
     result["images"] = [
         {
@@ -33,7 +33,12 @@ def sample_to_coco(sample_name: str, output: io.IOBase):
                 "id": i + 1,
                 "image_id": str(int(image_id)),
                 "category_id": int(category_id),
-                "bbox": [x * image_w, y * image_h, w * image_w, h * image_h],
+                "bbox": [
+                    (x - w / 2) * image_w,
+                    (y - h / 2) * image_h,
+                    w * image_w,
+                    h * image_h,
+                ],
                 "iscrowd": 0,
                 "area": w * image_w * h * image_h,
                 "ignore": 0,
