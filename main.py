f99ae949812b766e9eee51fa6451d52a47450db1
mihett05 <mihett05@yandex.ru>
2023-10-28 02:55:07 +0500

    Added merge

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from parser import coco
 
-for sample in ["video0", "video1", "video2"]:
+samples = ["video0", "video1", "video2"]
+for sample in samples:
     with open(f"dataset/{sample}.json", "w") as f:
         coco.sample_to_coco(sample, f)
+
+files = [open(f"dataset/{sample}.json") for sample in samples]
+with open(f"dataset/video.json", "w") as f:
+    coco.merge_coco(files, f)
+for file in files:
+    file.close()
